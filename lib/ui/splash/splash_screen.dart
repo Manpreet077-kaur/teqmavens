a20121a0936a1f48094e1adeb22dd239df30f385
arunchoudhary1017 <92821033+arunchoudhary1017@users.noreply.github.com>
2024-11-20 09:13:15 +0530

    //11/20

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:teqmavens_task/helper/app_color.dart';
+import 'package:teqmavens_task/helper/string_constants.dart';
+import 'package:teqmavens_task/utils/widgets/text_widget.dart';
 
 import '../../utils/utils.dart';
 import 'splash_controller.dart';
@@ -13,16 +15,28 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.whiteColor,
-      body:Center(
-        child:  AnimatedAlign(
-          alignment: controller.alignSplash,
-          duration: const Duration(seconds: 1),
-          curve: Curves.easeInOut,
-          child: UIData.imageView(
-           image:  'splash1.png',
-            width: Get.width * 0.4,
-          ),
-        ),
+      body:Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+         Row(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: [
+             TextWidget(StringConstants.weather, fontSize: 24.0,fontWeight: FontWeight.bold,),
+             Center(
+               child:  AnimatedAlign(
+                 alignment: controller.alignSplash,
+                 duration: const Duration(seconds: 1),
+                 curve: Curves.easeInOut,
+                 child: UIData.imageView(
+                   image:  'splash1.png',
+                   width: Get.width * 0.4,
+                 ),
+               ),
+             ),
+           ],
+         )
+        ],
       )
     );
   }
